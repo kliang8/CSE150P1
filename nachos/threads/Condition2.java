34ad457d3fb1843a878461db921502b3e2d11d66
kliang8 <kliang8@ucmerced.edu>
2018-11-04 00:55:03 -0700

    Update Condition2.java

--- nachos/threads/Condition2.java
+++ nachos/threads/Condition2.java
@@ -1,7 +1,7 @@
 package nachos.threads;
- 
+
 import nachos.machine.*;
- 
+
 /**
  * An implementation of condition variables that disables interrupt()s for
  * synchronization.
@@ -9,77 +9,64 @@ import nachos.machine.*;
  * <p>
  * You must implement this.
  *
- * @see nachos.threads.Condition
+ * @see	nachos.threads.Condition
  */
 public class Condition2 {
     /**
- 	* Allocate a new condition variable.
- 	*
- 	* @param  conditionLock  the lock associated with this condition
- 	*         	variable. The current thread must hold this
- 	*         	lock whenever it uses <tt>sleep()</tt>,
- 	*         	<tt>wake()</tt>, or <tt>wakeAll()</tt>.
- 	*/
+     * Allocate a new condition variable.
+     *
+     * @param	conditionLock	the lock associated with this condition
+     *				variable. The current thread must hold this
+     *				lock whenever it uses <tt>sleep()</tt>,
+     *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
+     */
     public Condition2(Lock conditionLock) {
 	this.conditionLock = conditionLock;
     }
- 
+
     /**
- 	* Atomically release the associated lock and go to sleep on this condition
- 	* variable until another thread wakes it using <tt>wake()</tt>. The
- 	* current thread must hold the associated lock. The thread will
- 	* automatically reacquire the lock before <tt>sleep()</tt> returns.
- 	*/
+     * Atomically release the associated lock and go to sleep on this condition
+     * variable until another thread wakes it using <tt>wake()</tt>. The
+     * current thread must hold the associated lock. The thread will
+     * automatically reacquire the lock before <tt>sleep()</tt> returns.
+     */
     public void sleep() {
 	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-
-	boolean x = Machine.interrupt().disable(); // Disable interrupts
-	KThread currentThread = KThread.currentThread(); // Get the current thread
-	waitQueue.waitForAccess(currentThread); // Wait for the currentThread to become accessible
-	conditionLock.release(); // conditionLock is released 
-	KThread.sleep(); // Thread is put to sleep
-	conditionLock.acquire(); // conditionLock is acquired
-	Machine.interrupt().restore(x);   // Restore interrupts
-  
+	conditionLock.release(); // Release lock
+	boolean tmp = Machine.interrupt().disable(); // Disable interrupts 
+	waitQueue.add(Kthread.currentThread()); // Add currentThread to waitQueue
+	KThread.currentThread().sleep(); // Put currentThread to sleep
+	Machine.interrupt().restore(tmp); // Restore interrupts
+	conditionLock.acquire(); // Acquire lock
     }
- 
+
     /**
- 	* Wake up at most one thread sleeping on this condition variable. The
- 	* current thread must hold the associated lock.
- 	*/
+     * Wake up at most one thread sleeping on this condition variable. The
+     * current thread must hold the associated lock.
+     */
     public void wake() {
 	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-    }
-   
-	boolean y = Machine.interrupt().disable(); // Disable interrupts
-	KThread tmpThread = waitQueue.nextThread(); // Get thread from waitQueue
-	if (tmpThread != null){ // If there is thread in queue
-   	tmpThread.ready(); // Ready Thread
+	boolean tmp = Machine.interrupt().disable(); // Disable interrupts
+	if (!waitQueue.isEmpty()) { // If waitQueue is not empty
+		KThread tmpThread = waitQueue.removeFirst(); // Get the first Thread in wait Queue
+		tmpThread.ready(); // Ready the thread
 	}
-	Machine.interrupt().restore(y); // Restore interrupts
-}
- 
+	Machine.interrupt().restore(tmp); // Restore interrupts
+		
+	}
+	
+
     /**
- 	* Wake up all threads sleeping on this condition variable. The current
- 	* thread must hold the associated lock.
- 	*/
+     * Wake up all threads sleeping on this condition variable. The current
+     * thread must hold the associated lock.
+     */
     public void wakeAll() {
 	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-    }
- 
-	boolean z = Machine.interrupt().disable(); // Disable interrupts
-	KThread tmpThread = waitQueue.nextThread(); // Get thread from waitQueue
-	tmpThread.ready(); // Ready Thread
-   	tmpThread = waitQueue.nextThread(); 
-   	
+	while(!waitQueue.isEmpty) { // While waitQueue is not empty
+		wake(); // Wake up all thread
 	}
-	Machine.interrupt().restore(z); //restore interrupts
- 
+	
     }
-   
+    private LinkedList<KThread> waitQueue = new LinkedList<KThread>(); // A link-list to store all the thread that are asleep
     private Lock conditionLock;
-    private ThreadQueue waitQueue = ThreadedKernel.scheduler.newThreadQueue(false);
 }
- 
-
-
