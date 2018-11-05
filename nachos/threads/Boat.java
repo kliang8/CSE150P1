7a87ea5a859a5cdf9529b1231a162eaf0009d3a3
marioOrtegaGarcia <mario.j.ortega.garcia@gmail.com>
2018-11-04 18:16:17 -0800

    fixed missing semicolons and extra equal sign

--- nachos/threads/Boat.java
+++ nachos/threads/Boat.java
@@ -4,31 +4,31 @@ import nachos.ag.BoatGrader;
 public class Boat
 {
     static BoatGrader bg;
-	
+
 	static Condition2 sleepOahuAdult;
 	static Condition2 sleepOahuChild;
 	static Condition2 sleepMolokaiChild;
 	static Condition2 sleepOnBoat;
-	
+
 	static Lock adultLock;
 	static Lock childLock;
 	static Lock boatLock;
 	static Lock molokaiLock;
-	
+
 	static boolean boatMolokai;
 	static boolean adultTurn;
-	
+
 	static int countOahuAdult;
 	static int countOahuChild;
 	static int countActiveChildren;
 	static int countBoat;
-	
+
 	static Semaphore finish;
-    
+
     public static void selfTest()
     {
 	BoatGrader b = new BoatGrader();
-	
+
 	System.out.println("\n ***Testing Boats with only 2 children***");
 	begin(0, 2, b);
 
@@ -46,10 +46,10 @@ public class Boat
 		bg = b;
 
 		// Instantiate global variables here
-		
+
 		// Create threads here. See section 3.4 of the Nachos for Java
 		// Walkthrough linked from the projects page.
-		
+
 		//Initialization of values
 		boatMolokai = false;
 		adultTurn = false;
@@ -57,27 +57,27 @@ public class Boat
 		countOahuChild = children;
 		countActiveChildren = 0;
 		countBoat = 0;
-		
+
 		//Initialization of locks and conditions
 		adultLock = new Lock();
 		childLock = new Lock();
 		molokaiLock = new Lock();
 		boatLock = new Lock();
-		
+
 		sleepOahuAdult = new Condition2(adultLock);
 		sleepOahuChild = new Condition2(childLock);
 		sleepMolokaiChild = new Condition2(molokaiLock);
-		sleepOnBoat = = new Condition2(boatLock);
-		
+		sleepOnBoat = new Condition2(boatLock);
+
 		Semaphore finish = new Semaphore(0);
-		
-		
+
+
 		Runnable a = new Runnable() {
 			public void run() {
 				adultItinerary();
 			}
 		};
-			
+
 		Runnable c = new Runnable() {
 			public void run() {
 				childItinerary();
@@ -89,7 +89,7 @@ public class Boat
 			t.setName("Sample Boat Thread");
 			t.fork();
 		}
-		
+
 		for(int i = 0; i < children; i++)
 		{
 			KThread t = new KThread(c);
@@ -103,9 +103,9 @@ public class Boat
     {
 		//Adult threads do not need a onMolokai check, because their threads end when they are on Molokai
 		//boolean onMolokai = false;
-		
+
 		//only one adult will be awake at a time, this is due to that adults are what wake children
-		adultLock.aquire()
+		adultLock.aquire();
 		//don't go to Molokai yet if the boat isn't there or if a child isn't on Molokai
 		while(boatMolokai || !adultTurn)
 		{
@@ -120,16 +120,16 @@ public class Boat
 		}
 		//Adult rows to Molokai
 
-		//The adults will know that there are no longer any children on Molokai, 
+		//The adults will know that there are no longer any children on Molokai,
 		//as the returning boat is the second child
-		adultTurn = false; 
-		
+		adultTurn = false;
+
 		countOahuAdult--;
 		boatMolokai = true;
 		bg.AdultRowToMolokai();
 		//wake a child to row the boat back to Oahu
 		sleepMolokaiChild.wake();
-		
+
 		adultLock.release();
     }
 
@@ -139,7 +139,7 @@ public class Boat
 		boolean onMolokai = false;
 		boolean finalVoyage = false;
 		boolean passenger;
-		
+
 		while(true)
 		{
 			passenger = false;
@@ -154,12 +154,12 @@ public class Boat
 				}
 				countActiveChildren++;
 				childLock.release();
-				
+
 				//If there are only 2 children left on the island, then this is the final voyage
 				//This check is done before sailing, to simulate only information known to people on the island
 				if(countOahuChild <= 2 && countOahuAdults <= 0)
 					finalVoyage = true;
-				
+
 				boatlock.aquire();
 				//If there are zero children waiting on the boat...
 				if(countBoat == 0)
@@ -188,15 +188,15 @@ public class Boat
 					sleepOnBoat.wake();
 				}
 				boatLock.release();
-				
+
 				//If there are still adults on the island, then they will take the boat next
 				if(countOahuAdult > 0)
-						adultTurn=true
-				
+						adultTurn=true;
+
 				//Set counters and send messages that the two boat children are now in Molokai
 				countOahuChild--;
 				onMolokai = true;
-				//Okay to decrement active children without lock, because the 
+				//Okay to decrement active children without lock, because the
 				// while loop is still true with the boat now at Molokai.
 				boatMolokai = true;
 				countActiveChildren--;
@@ -205,35 +205,35 @@ public class Boat
 					bg.ChildRideToMolokai();
 				else
 					bg.ChildRowToMolokai();
-				
+
 				//if the child is the last one off the boat, they will instead return back to Oahu
 				//otherwise they will sleep
 				molokaiLock.aquire();
 				countBoat--;
-				if(countBoat > 0)	
+				if(countBoat > 0)
 				{
 					sleepMolokaiChild.sleep();
 				}
 				molokaiLock.release();
 			}
-			
+
 			//finishes the simulation if no one was on the island after leaving
 			//This is done after the lock check to make sure both the row and ride messages were sent
-			if(finalVoyage)	
+			if(finalVoyage)
 			{
 				finish.V();
 				break;
 			}
-				
+
 			if(onMolokai)
 			{
 				//A child rows back to Oahu by themselves.
 				countOahuChild++;
 				onMolokai = false;
 				boatMolokai = false;
-				
+
 				bg.ChildRowToOahu();
-				
+
 				//If there are adults on  the island and still a child on Molokai, then the adult should go
 				//Otherwise wake a child to go to the island again
 				if(countOahuAdult > 0 && adultTurn)
