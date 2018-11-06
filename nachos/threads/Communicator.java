d2ee61a8d4160fd994e8426360fb35ffb4815fc9
cowfish13 <43792333+cowfish13@users.noreply.github.com>
2018-11-06 12:41:39 -0800

    Update Communicator.java

--- nachos/threads/Communicator.java
+++ nachos/threads/Communicator.java
@@ -80,7 +80,7 @@ public class Communicator {
         //         cSpeaker.wake();
         // }
         while(validMessage == false) {
-                if (speakerCount > 0) cSpeaker.wakeAll();
+                cSpeaker.wake();
     		cListener.sleep();
     	}
         // Save word and Reset the word
